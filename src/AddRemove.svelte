24dd7c8c99206d05a735f245302920e75c6d8ce5
David Callizaya <davidcallizaya@gmail.com>
2022-05-06 05:39:01 -0400

    Add Remove control

---
+++ src/AddRemove.svelte
@@ -0,0 +1,5 @@
+<div class="plus-minus-control">
+    <a href="#" class="plus-minus">+</a>
+    <input placeholder="0" type="number" style="width: 2rem; font-size: 2rem;padding: 0.15rem;" />
+    <a href="#" class="plus-minus">-</a>
+</div>
