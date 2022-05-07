2c7810dfc48df4a123635c125c700ba5e7304b25
David Callizaya <davidcallizaya@gmail.com>
2022-05-07 05:40:42 -0400

    Refactoring

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,8 @@
 <script>
+	import AddRemove from "./AddRemove.svelte";
+
 	export let name;
 </script>
 
-<main>
-	<div class="logo-container">
-		<img src="images/abierto.svg" alt="logo" class="logo">
-		<button>Reserve ahora</button>
-	</div>
-	<picture>
-		<img src="images/panel1.jpg" alt="fondo" class="fondo">
-	</picture>
-</main>
-
 <style>
-	.logo-container {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: calc(100vh - 200px);
-		gap: 4rem;
-	}
-	.fondo {
-		width: 100%;
-	}
 </style>
